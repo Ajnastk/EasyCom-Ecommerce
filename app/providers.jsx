fc6567e7f9908cf60f44cc0f1ff959a07805ed36
ziyad <ziyadmuhammad9696@gmail.com>
2025-06-18 22:01:40 +0530

    redux updated

--- app/providers.jsx
+++ app/providers.jsx
@@ -2,12 +2,13 @@
 
 import { SessionProvider } from "next-auth/react";
 import { Provider as ReduxProvider } from "react-redux";
-import { store } from "./store";
+import { store } from "./store/store";
 
 export function Providers({ children }) {
   return (
     <SessionProvider>
       <ReduxProvider store={store}>
+        {/* {console.log("Redux Store: ",store)} */}
         {children}
       </ReduxProvider>
     </SessionProvider>
